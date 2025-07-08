05b6ad22fb143f48c83b07473dd3eed7015856be
Deepraj Bajagain <bungydeep@gmail.com>
2025-07-08 11:56:45 -0400

    feat: Add employee repository with basic CRUD support

---
+++ ems-backend/src/main/java/com/bungydeep/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.bungydeep.repository;
+
+import com.bungydeep.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
