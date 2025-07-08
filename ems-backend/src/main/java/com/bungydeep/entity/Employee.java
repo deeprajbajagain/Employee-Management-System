c847a80ab526a0d5e4fff1ee79a3bcad611085b3
Deepraj Bajagain <bungydeep@gmail.com>
2025-07-08 11:43:00 -0400

    Add Employee entity with JPA annotations

---
+++ ems-backend/src/main/java/com/bungydeep/entity/Employee.java
@@ -0,0 +1,24 @@
+package com.bungydeep.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+}
+
